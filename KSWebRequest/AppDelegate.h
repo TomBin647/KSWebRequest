ab8894261ba31cfa453a9ee782684efa69c230e4
Howard <bing.hao1982@gmail.com>
2015-09-10 09:39:55 +0800

    Initial Commit

---
+++ KSWebRequest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KSWebRequest
+//
+//  Created by bing.hao on 15/9/10.
+//  Copyright (c) 2015年 Bing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KSWebRequest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KSWebRequest
+//
+//  Created by bing.hao on 15/9/10.
+//  Copyright (c) 2015年 Bing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
